ffaf54bccd3da4e2f7a562bcc977313ff12951a4
Alexis <35.alexis.pele@gmail.com>
2024-04-27 16:54:35 +0200

    Added scroll wheel navigation to yearly layout

--- src/components/calendar/state/yearly/Layout.vue
+++ src/components/calendar/state/yearly/Layout.vue
@@ -1,12 +1,31 @@
 <script lang="ts" setup>
 import { useCalendar } from '@/stores/CalendarStore'
+import { useThrottleFn } from '@vueuse/core'
+
 import MonthTile from './MonthTile.vue'
 
-const { staticConfig } = useCalendar()
+const { staticConfig, decrementYear, incrementYear } = useCalendar()
+
+function handleWheel(e: WheelEvent) {
+  const isMovingUp = e.deltaY < 0
+  if (isMovingUp) {
+    moveCalendarLeft()
+  } else {
+    moveCalendarRight()
+  }
+}
+
+const moveCalendarLeft = useThrottleFn(() => {
+  decrementYear()
+}, 100)
+
+const moveCalendarRight = useThrottleFn(() => {
+  incrementYear()
+}, 100)
 </script>
 
 <template>
-  <div class="container mt-[10vh] mb-auto">
+  <div class="container mt-[10vh] mb-auto" @wheel="handleWheel">
     <div ref="test" class="grid grid-cols-5 gap-x-8 gap-y-16">
       <MonthTile
         v-for="month in staticConfig.monthsPerYear"
