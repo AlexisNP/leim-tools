4e9bc4137b0864fa8197d363037eb67a86377659
Alexis <35.alexis.pele@gmail.com>
2024-05-12 22:24:17 +0200

    Added back meta from old index.html

--- app.vue
+++ app.vue
@@ -4,7 +4,26 @@ import { ConfigProvider } from 'radix-vue'
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} — TTTools` : 'TTTools';
-  }
+  },
+  meta: [
+    { name: "charset", content: "UTF-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+    { name: "author", content: "Alexis Pelé" },
+    { name: "description", content: "Tools destined to players and game master, helping them visualize their worlds better." },
+    { name: "generator", content: "Nuxt" },
+    { name: "msapplication-TileColor", content: "00aba9" },
+    { name: "theme-color", content: "00aba9" },
+    { name: "og:type", content: "tabletop-tools" },
+    { name: "og:url", content: "ttt.alexcreates.fr" },
+    { name: 'robots', content: 'noindex, nofollow'}
+  ],
+  link: [
+    { rel: 'apple-touch-icon', sizes: "76x76", href: '/apple-touch-icon.png' },
+    { rel: 'icon', sizes: "32x32", type: 'image/png', href: '/favicon-32x32.png' },
+    { rel: 'icon', sizes: "16x16", type: 'image/png', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#6595b4' },
+  ]
 })
 
 const useIdFunction = () => useId()
