d2295024bd49c93e6f0e64626695a43f0cfa3b84
Alexis <35.alexis.pele@gmail.com>
2025-03-28 12:12:37 +0100

    Changed span to kbd

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -28,7 +28,7 @@ const props = withDefaults(defineProps<Props>(), {
       v-if="props.searchSlash"
       class="h-4 p-1 ml-1 grid place-items-center text-[0.7em] leading-none font-semibold text-slate-500 bg-slate-100 border-slate-300 border-[1px] rounded-[3px] shadow-sm group-hover:text-slate-600 group-hover:bg-slate-200 group-hover:border-slate-400 transition-colors"
     >
-      <span class="-mt-[2px]">CTRL + :</span>
+      <kbd class="-mt-[2px]">CTRL + :</kbd>
     </span>
   </Primitive>
 </template>
