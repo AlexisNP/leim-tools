09cd25ce333bd82326f43c05189f6eeef7257a5d
Alexis <35.alexis.pele@gmail.com>
2025-05-07 13:18:01 +0200

    Removed duplicate character in regex

--- components/calendar/SearchMode.ts
+++ components/calendar/SearchMode.ts
@@ -1,3 +1,3 @@
-export const searchUnifier = new RegExp(/[^a-zA-Z0-9\-'']/g)
+export const searchUnifier = new RegExp(/[^a-zA-Z0-9\-']/g)
 
 export type SearchMode = "characters" | "events" | undefined
